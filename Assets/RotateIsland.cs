19f95a8765fef5a5e717add72608f1937a9b9899
starryahri <ahrianicholas@gmail.com>
2021-08-30 00:25:56 -0700

    Added main menu with camera movement

---
+++ Assets/RotateIsland.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateIsland : MonoBehaviour
+{
+    public float rotateSpeed = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, rotateSpeed, 0f));
+
+    }
+}
